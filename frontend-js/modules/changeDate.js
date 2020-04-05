b487752ab5d2214a81a33d136f42097273a8f301
GallantJohny <hannsg14@gmail.com>
2020-04-05 21:00:56 +0200

    Worked on front end date rendering logic.

--- frontend-js/modules/changeDate.js
+++ frontend-js/modules/changeDate.js
@@ -14,23 +14,24 @@ export default class ChangeDate {
         this.isLoading = true;
         this.addEvents();
         this.handleLoadingIcon();
-        this.renderCosts();
+        this.renderCosts(new Date());
     }
 
     // Events
     addEvents() {
-        this.prevWeekBtn.addEventListener("click", () => this.renderPrevWeek());
-        this.nextWeekBtn.addEventListener("click", () => this.renderNextWeek());
+        this.prevWeekBtn.addEventListener("click", () => this.renderPrevWeek(), { once: true });
+        this.nextWeekBtn.addEventListener("click", () => this.renderNextWeek(), { once: true });
     }
 
     // Methods
-    renderCosts() {
+    renderCosts(date) {
         let expenses = [];
         setTimeout(() => {
-            axios.post(`/fetchExpenses`, { date: new Date() }).then((response) => {
+            axios.post(`/fetchExpenses`, { date: date }).then((response) => {
+                this.costsContainer.innerHTML = "";
                 expenses = response.data[0];
                 console.log(expenses);
-                if (expenses) {
+                if (expenses.length) {
                     expenses.forEach((expense) => {
                         this.costsContainer.insertAdjacentHTML('beforeend', `
                         <li class="flex-display-center cost-item">
@@ -42,15 +43,25 @@ export default class ChangeDate {
                     });
                     this.totalSpent.innerText = response.data[1];
                     this.remaining.innerText = response.data[2];
-                    this.isLoading = false;
-                    this.handleLoadingIcon();
                 } else {
-                    this.costsContainer.insertAdjacentHTML("beforeend", "<li> Looks like your expenditure is empty. </li>");
-                    this.isLoading = false;
-                    this.handleLoadingIcon();
+                    this.costsContainer.insertAdjacentHTML('beforeend', '<li class="padding-15"> No expenses this week. </li>');
                 }
+
+                this.isLoading = false;
+                this.changeDateToggleBtnState();
+                this.handleLoadingIcon();
             }).catch((e) => console.log('[changeDate.js]: Post request failed'));
-        }, 2000);
+        }, 500);
+    }
+
+    changeDateToggleBtnState() {
+        if (!this.isLoading) {
+            this.prevWeekBtn.classList.remove('disabled');
+            this.nextWeekBtn.classList.remove('disabled');
+        } else {
+            this.prevWeekBtn.classList.add('disabled');
+            this.nextWeekBtn.classList.add('disabled');
+        }
     }
 
     handleLoadingIcon() {
@@ -63,19 +74,67 @@ export default class ChangeDate {
         }
     }
 
-    renderNextWeek() {
-        const startDateArr = this.startDate.innerText.trim().split('.');
-        const endDateArr = this.endDate.innerText.trim().split('.');
+    renderNextWeek(a) {
+        console.log(a);
+        this.isLoading = true;
+        this.changeDateToggleBtnState();
+        this.handleLoadingIcon();
 
-        const startDate = new Date(startDateArr[0], startDateArr[1] - 1, startDateArr[2] + 7);
-        const endDate = new Date(endDateArr[0], endDateArr[1] - 1, endDateArr[2] + 7);
+        const startDate = this.returnWeekElapsedDate(this.startDate.innerText.trim().split('.'));
+        const endDate = this.returnWeekElapsedDate(this.endDate.innerText.trim().split('.'));
 
-        console.log(startDate, endDate);
+        this.renderCosts(startDate);
+        this.nextWeekBtn.addEventListener("click", () => this.renderNextWeek(), { once: true });
 
-
+        this.startDate.innerText = this.returnFormattedDate(startDate);
+        this.endDate.innerText = this.returnFormattedDate(endDate);
     }
 
     renderPrevWeek() {
+        this.isLoading = true;
+        this.changeDateToggleBtnState();
+        this.handleLoadingIcon();
 
+        const startDate = this.returnPrevWeekDate(this.startDate.innerText.trim().split('.'));
+        const endDate = this.returnPrevWeekDate(this.endDate.innerText.trim().split('.'));
+
+        this.renderCosts(startDate);
+        this.prevWeekBtn.addEventListener("click", () => this.renderPrevWeek(), { once: true });
+
+        this.startDate.innerText = this.returnFormattedDate(startDate);
+        this.endDate.innerText = this.returnFormattedDate(endDate);
+    }
+
+    returnWeekElapsedDate(dateArray) {
+        const weekInteger = 7 * 24 * 60 * 60 * 1000;
+        const date = new Date(dateArray[0], dateArray[1] - 1, dateArray[2]);
+        const weekPassedDate = new Date(date.valueOf() + weekInteger);
+
+        return weekPassedDate;
+    }
+
+    returnPrevWeekDate(dateArray) {
+        const weekInteger = 7 * 24 * 60 * 60 * 1000;
+        const date = new Date(dateArray[0], dateArray[1] - 1, dateArray[2]);
+        const prevWeekDate = new Date(date.valueOf() - weekInteger);
+
+        return prevWeekDate;
+    }
+
+    returnFormattedDate(date) {
+        const startYear = `${date.getFullYear()}`;
+        const startMonth = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
+        const startDay = date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`;
+
+        return `${startYear}.${startMonth}.${startDay}`;
+    }
+
+    returnClosestMonday(date) {
+        const oneDay = 24 * 60 * 60 * 1000;
+        let newDate = new Date(date);
+
+        while (newDate.getDay() !== 1){
+            newDate = new Date(newDate.valueOf() - oneDay);
+        }
     }
 }
